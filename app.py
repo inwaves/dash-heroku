38c18f2f6867b22e7109aefe3fd3fee37fcb69ca
Andrei <inwaves@live.com>
2020-11-10 09:51:51 +0200

    fixed server variable

--- app.py
+++ app.py
@@ -1,21 +1,33 @@
 import dash
 import dash_core_components as dcc
 import dash_html_components as html
+import plotly.express as px
+import pandas as pd
 
-external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
-
+external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwqP.css']
 app = dash.Dash(__name__, external_stylesheets=external_stylesheets)
 
 server = app.server
 
-top_markdown_text = '''
-This is my first deployed app
-'''
+df = pd.DataFrame({
+    "Fruit": ["Apples", "Oranges", "Bananas", "Apples", "Oranges", "Bananas"],
+    "Amount": [4, 1, 2, 2, 4, 5],
+    "City": ["SF", "SF", "SF", "Montreal", "Montreal", "Montreal"]
+})
 
-app.layout = html.Div([
+fig = px.bar(df, x='Fruit', y='Amount', color='City', barmode='group')
 
-    dcc.Markdown(children=top_markdown_text),
+app.layout = html.Div(children=[
+    html.H1(children='Hello Dash'),
 
+    html.Div(children='''
+        Dash: A web application framework for Python.
+    '''),
+
+    dcc.Graph(
+        id='example-graph',
+        figure=fig
+    )
 ])
 
 if __name__ == '__main__':
